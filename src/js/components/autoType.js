9291179dadeb1fb0eb7ca11c6640f204efbbb2b5
CarlLinusHedlund <carl.hedlund@stud.noroff.no>
2022-11-29 17:22:14 +0100

    added typed js and fixed inner on header and main

---
+++ src/js/components/autoType.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+var textDom = document.querySelector('.auto-typed');
+console.log(textDom);
+
+var typed = new Typed('.auto-type', {
+    strings: ['ART ^1200', 'CAR ^1200', 'CHAIR ^1200', 'BIKE ^1200', 'BAG ^1200', 'BOAT ^1200', 'WINE ^1200'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    cursorChar: '',
+});
