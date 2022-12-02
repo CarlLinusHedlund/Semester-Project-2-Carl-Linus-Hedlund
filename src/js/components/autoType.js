46bb4d56eadf018d3441f4cdccdd0a9caa8883cd
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2022-12-02 19:59:52 +0100

    didnt manage the autologin to work. But added some new styling

--- src/js/components/autoType.js
+++ src/js/components/autoType.js
@@ -1,7 +1,6 @@
 import Typed from 'typed.js';
 
 var textDom = document.querySelector('.auto-typed');
-console.log(textDom);
 
 var typed = new Typed('.auto-type', {
     strings: ['ART ^1200', 'CAR ^1200', 'CHAIR ^1200', 'BIKE ^1200', 'BAG ^1200', 'BOAT ^1200', 'WINE ^1200'],
