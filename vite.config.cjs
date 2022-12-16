23ae716104789c959f226a10ae96cc8f2c3faedd
CarlLinusHedlund <carl.hedlund@stud.noroff.no>
2022-12-16 22:12:05 +0100

    vite config update

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,18 +1,23 @@
 const { resolve } = require('path');
 
 export default {
-  base: '/Semester-Project-2-Carl-Linus-Hedlund/',
   root: resolve(__dirname, 'src'),
-  resolve: {
-    main: resolve(__dirname, 'index.html'),
-    signup: resolve(__dirname, 'signup/signup.html'),
-    signIN: resolve(__dirname, 'signin/signIn.html'),
-    listings: resolve(__dirname, 'listings.html'),
-    specificProduct: resolve(__dirname, 'specificProduct.html'),
-    profile: resolve(__dirname, 'dashboard/dashboard.html'),
-    wallet: resolve(__dirname, 'dashboard/wallet.html'),
-    overview: resolve(__dirname, 'dashboard/overview.html'),
-    makeAList: resolve(__dirname, 'dashboard/makeAList.html'),
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        signup: resolve(__dirname, 'src/signup/signup.html'),
+        signIN: resolve(__dirname, 'src/signin/signIn.html'),
+        listings: resolve(__dirname, 'src/listings.html'),
+        specificProduct: resolve(__dirname, 'src/specificProduct.html'),
+        profile: resolve(__dirname, 'src/dashboard/dashboard.html'),
+        wallet: resolve(__dirname, 'src/dashboard/wallet.html'),
+        overview: resolve(__dirname, 'src/dashboard/overview.html'),
+        makeAList: resolve(__dirname, 'src/dashboard/makeAList.html'),
+      },
+    },
   },
   server: {
     port: 8080,
